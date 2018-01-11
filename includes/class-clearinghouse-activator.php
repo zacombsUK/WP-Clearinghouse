994e7cd54e3ed12118164cbb5e93e163d25aa4ce
zacombsUK <zac.combs@uky.edu>
2018-01-11 12:56:19 -0500

    Add files via upload

---
+++ includes/class-clearinghouse-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://zacombs.com/
+ * @since      1.0.0
+ *
+ * @package    Clearinghouse
+ * @subpackage Clearinghouse/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Clearinghouse
+ * @subpackage Clearinghouse/includes
+ * @author     Zachary Combs <zac.combs@uky.edu>
+ */
+class Clearinghouse_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-clearinghouse-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://zacombs.com/
+ * @since      1.0.0
+ *
+ * @package    Clearinghouse
+ * @subpackage Clearinghouse/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Clearinghouse
+ * @subpackage Clearinghouse/includes
+ * @author     Zachary Combs <zac.combs@uky.edu>
+ */
+class Clearinghouse_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
